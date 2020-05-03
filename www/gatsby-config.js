2c5d049e1134562b2c60f29e5ebfd1cea47c8a7b
Alexandru-Dan Pop <alexandrudanpop@gmail.com>
2020-05-03 15:32:05 +0300

    use gatsby-plugin-gtag for GA

--- www/gatsby-config.js
+++ www/gatsby-config.js
@@ -74,26 +74,15 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-google-gtag`,
+      resolve: `gatsby-plugin-gtag`,
       options: {
-        trackingIds: [
-          `G-2X1YWGET59`, // Google Analytics / GA
-        ],
-        // This object gets passed directly to the gtag config command
-        // This config will be shared across all trackingIds
-        gtagConfig: {
-          anonymize_ip: true,
-          cookie_expires: 0,
-          // send_page_view: true,
-          cookie_name: `google-gtag`,
-        },
-        // This object is used for configuration specific to this plugin
-        pluginConfig: {
-          // Puts tracking script in the head instead of the body
-          head: true,
-          // Setting this parameter is also optional
-          respectDNT: false,
-        },
+        // your google analytics tracking id
+        trackingId: `G-2X1YWGET59`,
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // enable ip anonymization
+        anonymize: true,
+        send_page_view: true,
       },
     },
   ],
